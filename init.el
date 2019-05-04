ab2d06a7191329e7b3a000cf6935f9ad0fd8cb40
Simon Jefford <simon.jefford@fourth.com>
2019-05-04 22:27:34 +0100

    a start

---
+++ init.el
@@ -0,0 +1,36 @@
+;; MELPA setup
+(require 'package)
+
+(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;; TODO
+;; custom stuff into a separate file
+;; backup files into a sane place
+;; magit
+;; Ctrl-W
+;; helm(?)
+;; kill ring stuff
+;; go
+;; rust
+;; markdown
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
