cf60ef109b4c4354a70b688842af012a2c88da50
Simon Jefford <simon.jefford@fourth.com>
2019-05-05 10:19:13 +0100

    install projectile

--- init.el
+++ init.el
@@ -29,6 +29,9 @@
 (use-package magit
   :ensure t)
 
+(use-package projectile
+  :ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
