30da94afffff06ed4c3388f77951cc977869435d
Simon Jefford <simon.jefford@fourth.com>
2019-05-06 19:32:17 +0100

    configure ibuffer

--- init.el
+++ init.el
@@ -137,6 +137,62 @@ Windows external keyboard from time to time."
 
 (server-start)
 
+(bind-key "C-x C-b" 'ibuffer)
+
+(setq ibuffer-saved-filter-groups
+    '(("home"
+          ("Emacs-lisp" (or (filename . "\.el$")
+                            (name . "\*scratch")))
+          ("Clojure" (mode . clojure-mode))
+          ("Ruby/Rails" (or (mode . ruby-mode)
+                            (mode . haml-mode)
+                            (mode . less-css-mode)))
+          ("CSS" (or (mode . css-mode)
+                     (mode . scss-mode)))
+          (".NET" (mode . csharp-mode))
+          ("HTML / HTML Templates" (mode . web-mode))
+          ("Go" (mode . go-mode))
+          ("Rust" (mode . rust-mode))
+          ("Terminals" (mode . term-mode))
+          ("Scripts" (mode . sh-mode))
+          ("Inf As Code" (mode . terraform-mode))
+          ("Build files" (or (mode . dockerfile-mode)
+                             (mode . makefile-mode)
+                             (mode . makefile-bsdmake-mode)))
+          ("JS" (or (mode . js2-mode)
+                    (mode . js-mode)
+                    (mode . coffee-mode)))
+          ("Docs" (or (mode . markdown-mode)
+                      (mode . showoff-mode)
+                      (mode . gfm-mode)))
+          ("Configuration" (or (mode . json-mode)
+                               (mode . conf-mode)
+                               (mode . yaml-mode)
+                               (mode . conf-unix-mode)))
+          ("Code"  (filename . "code"))
+          ("REPL" (or (mode . inferior-lisp-mode)
+                      (mode . slime-repl-mode)))
+
+          ("Shells" (or (mode . eshell-mode)))
+          ("Magit" (name . "\*magit"))
+          ("Org" (mode . org-mode))
+          ("ERC" (mode . erc-mode))
+          ("Dirs" (mode . dired-mode))
+          ("Customize" (mode . Custom-mode))
+          ("Help" (or (name . "\*Help\*")
+                      (name . "\*Apropos\*")
+                      (name . "\*info\*")))
+          ("Scala" (mode . scala-mode))
+          ("Man pages" (mode . Man-mode)))))
+
+(add-hook 'ibuffer-mode-hook
+    '(lambda ()
+         (ibuffer-switch-to-saved-filter-groups "home")
+         (ibuffer-auto-mode 1)))
+
+(setq ibuffer-show-empty-filter-groups nil)
+(add-to-list 'ibuffer-never-show-predicates "^\\*")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
