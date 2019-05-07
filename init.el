a4c59183379c34521ede54c50017a4de157b0557
Simon Jefford <simon.jefford@fourth.com>
2019-05-07 10:20:52 +0100

    isearch-occur to C-o

--- init.el
+++ init.el
@@ -193,15 +193,6 @@ Windows external keyboard from time to time."
 (setq ibuffer-show-empty-filter-groups nil)
 (add-to-list 'ibuffer-never-show-predicates "^\\*")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
