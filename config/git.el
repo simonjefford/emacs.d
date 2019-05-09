04fbea0eb6fb15758f7590719347996aa263e03b
Simon Jefford <simon.jefford@fourth.com>
2019-05-09 15:40:31 +0100

    magit fullscreen niceness

--- config/git.el
+++ config/git.el
@@ -1,9 +1,17 @@
 (unbind-key "s-m")
 
+(defun magit-quit-session()
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)
-	 ("s-m b" . magit-blame-addition)))
+	 ("s-m b" . magit-blame-addition)
+	 :map magit-status-mode-map
+	 ("q" . 'magit-quit-session))
+  )
 
 (use-package git-timemachine
   :ensure t)
@@ -15,5 +23,10 @@
     (global-diff-hl-mode +1)
     (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
     (add-hook 'dired-mode-hook 'diff-hl-dired-mode)))
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
 
 (provide 'git)
