ef35c1a6a60894c171458b738a3e79542587e962
Simon Jefford <simon.jefford@fourth.com>
2019-05-21 21:44:44 +0100

    helm rather than ivy

---
+++ config/helm-setup.el
@@ -0,0 +1,33 @@
+(use-package helm
+  :ensure t
+  :bind (("M-x" . 'helm-M-x)
+	 ("C-x C-m" . 'helm-M-x)
+	 ("M-y" . 'helm-show-kill-ring)
+	 ("C-x b" . 'helm-mini)
+	 ("C-x C-f" . 'helm-find-files)
+	 ("C-h f" . 'helm-apropos)
+	 ("C-h r" . 'helm-info-emacs)
+	 ("C-h C-l" . 'helm-locate-library)
+	 ("C-c h" . 'helm-command-prefix))
+  :init (progn
+	  (setq helm-split-window-inside-p            t
+		helm-buffers-fuzzy-matching           t
+		helm-move-to-line-cycle-in-source     t
+		helm-ff-search-library-in-sexp        t
+		helm-ff-file-name-history-use-recentf t))
+  :config (require 'helm-config)
+  :bind (:map helm-command-map
+	      ("o" . 'helm-occur)))
+
+(use-package helm-projectile
+  :ensure t
+  :config (progn
+	    (helm-projectile-on)))
+
+(use-package helm-ag
+  :ensure t)
+
+(use-package helm-descbinds
+  :ensure t)
+
+(provide 'helm-setup)
